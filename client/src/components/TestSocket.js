0b4881750a3921ca03458897022be838b7f2043b
NiRuaG <gaurin1107@gmail.com>
2019-02-08 23:51:36 -0600

    component for testing sockets

--- client/src/components/TestSocket.js
+++ client/src/components/TestSocket.js
@@ -4,19 +4,58 @@ import openSocket from 'socket.io-client';
 
 class TestSocket extends Component {
   state = {
-    socket: openSocket('/')
+    socket: openSocket('/'),
+    connection: 'connecting...',
+    gameReady: false,
+    messages: []
   }
 
   componentWillMount() {
     //! needs a state refresh after connection is finished
     this.state.socket.on('connect', () => {
-      this.setState({})
+      this.setState({ connection: 'connected' })
+    });
+
+
+    this.state.socket.on('disconnect', () => {
+      this.setState({ connection: 'no connection' })
+    });
+
+    this.state.socket.on('seat', index => {
+      this.setState({ seatedAt: index+1 });
+    });
+
+    this.state.socket.on('ready', readyStatus => {
+      this.setState({ gameReady: readyStatus })
     })
+
+    this.state.socket.on('message', message => {
+      const messages = [...this.state.messages];
+      messages.unshift(message);
+      this.setState({ messages });
+    })
+
   }
+
+  clickIt = () => this.state.socket.emit('click');
 
   render() {
     return (
-      <p>{this.state.socket.id}</p>
+      <>
+        <p>{this.state.connection}</p> 
+
+        {this.state.seatedAt &&
+          <>
+            <p>{this.state.seatedAt && `I am Player ${this.state.seatedAt}`}</p>
+            <p>{this.state.gameReady ? "READY!" : "..hold on.."}</p>
+            <button onClick={this.clickIt}>Click me</button>
+            {this.state.messages.map((msg,idx) => (
+              <p key={`msg-${idx}`}>{msg} has something to say</p>
+            ))}
+          </>
+        }
+       
+      </>
     );
   }
 }
