309f71e9b6eab81a920c8debf03752b531f2fec1
NiRuaG <gaurin1107@gmail.com>
2019-02-08 18:20:34 -0600

    reconfigure proxy, upgrades to websocket protocol

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware')
     
 module.exports = function(app) {
-  app.use(proxy('/socket', {
+  app.use(proxy('/socket.io', {
     target: 'http://localhost:3001/',
     ws: true
   }))
