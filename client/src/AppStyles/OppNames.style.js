23ca7976b561e390729c193fb757fe599758044a
NiRuaG <gaurin1107@gmail.com>
2019-02-09 22:47:41 -0600

    center fix for Chrome

--- client/src/AppStyles/OppNames.style.js
+++ client/src/AppStyles/OppNames.style.js
@@ -23,6 +23,11 @@ const SideName = styled(OppNameBase)`
   writing-mode: vertical-lr;
   text-orientation: upright;
   padding: 0.5rem 0.2rem;
+
+  /* Chrome (72.0.3626.96) would not vertically center name without this */
+  /* FireFox (65.0) would without this. But possibly not per spec due to position: absolute?
+  https://www.w3.org/TR/css-flexbox-1/#abspos-items */
+  top: 50%; transform: translate(0,-50%);
 `;
 
 const LeftName = styled(SideName)`
