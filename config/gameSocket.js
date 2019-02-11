7d3eab7d96656cb081f75c3ac14043c974f68b0f
NiRuaG <gaurin1107@gmail.com>
2019-02-11 12:28:05 -0600

    board emits (with rotations)

--- config/gameSocket.js
+++ config/gameSocket.js
@@ -3,6 +3,80 @@ const playerConnMap = new Map();
 
 const game = require('./../gamelogic');
 
+const BOARD_ROT_MAPs = [
+  //% Player 1 is basis of BOARD perspective
+
+  //* Player 2 perspective
+  [ 42, 35, 28, 21, 14,  7,  0,
+    43, 36, 29, 22, 15,  8,  1,
+    44, 37, 30, 23, 16,  9,  2,
+    45, 38, 31, 24, 17, 10,  3,
+    46, 39, 32, 25, 18, 11,  4,
+    47, 40, 33, 26, 19, 12,  5,
+    48, 41, 34, 27, 20, 13,  6, ],
+
+  //* Player 3 perspective
+  [ 48, 47, 46, 45, 44, 43, 42,
+    41, 40, 39, 38, 37, 36, 35,
+    34, 33, 32, 31, 30, 29, 28,
+    27, 26, 25, 24, 23, 22, 21,
+    20, 19, 18, 17, 16, 15, 14,
+    13, 12, 11, 10,  9,  8,  7,
+     6,  5,  4,  3,  2,  1,  0, ],
+
+  //* Player 4 perspective
+  [ 6, 13, 20, 27, 34, 41, 48,
+    5, 12, 19, 26, 33, 40, 47,
+    4, 11, 18, 25, 32, 39, 46,
+    3, 10, 17, 24, 31, 38, 45,
+    2,  9, 16, 23, 30, 37, 44,
+    1,  8, 15, 22, 29, 36, 43,
+    0,  7, 14, 21, 28, 35, 42, ],
+];
+
+const TILE_ROT_MAPs = [
+  //% Player 1 is basis of TILE perspective
+  //% last index is for special characteristics
+
+  //* Player 2 perspective
+  [1, 2, 3, 0, 4],
+
+  //* Player 3 perspective
+  [2, 3, 0, 1, 4],
+
+  //* Player 4 perspective
+  [3, 0, 1, 2, 4],
+]
+
+function emitBoard() {
+  //% Player 1 is basis of board perspective
+  playerConnections[0].emit('board', game.board);
+
+
+  playerConnections.slice(1).forEach((conn, connIdx) => {
+    const theirTileMapper = TILE_ROT_MAPs[connIdx]; // stored to maintain throughout board mapping
+
+    const theirBoard = BOARD_ROT_MAPs[connIdx].map(boardRot => {
+
+      // see if there is any tile at that location, and if so rotate its colors
+      let theirTile = game.board[boardRot] && 
+        theirTileMapper.map(tileRot => game.board[boardRot][tileRot])
+
+      return theirTile;
+
+    });
+
+    conn.emit('board', theirBoard);
+
+  });
+
+}
+
+function startTheGame(socket) {
+  game.setup();
+  emitBoard();
+}
+
 
 module.exports = (socket) => {
 
@@ -28,13 +102,12 @@ module.exports = (socket) => {
     socket.emit('players', playerConnections.map(conn => !!conn))
 
     //* All Players Seated
-    if (playerConnections.every(conn => conn !== null)) { 
+    if (playerConnections.every(conn => conn !== null)) {
       console.log("game is ready!")
       socket.emit('ready', true);
 
       //! TODO: don't always restart the game just due to having a 4th player
-      game.setup();
-      socket.emit('tile', [0,1,2,3], 3, 3);
+      startTheGame(socket, playerConnections);
     }
 
 
