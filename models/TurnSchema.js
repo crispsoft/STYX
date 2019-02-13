0d65737a62ddfc0b80c296713ae823d849d6e510
NiRuaG <gaurin1107@gmail.com>
2019-02-13 10:51:33 -0600

    Turn schema shouldn't need _id

--- models/TurnSchema.js
+++ models/TurnSchema.js
@@ -2,21 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-module.exports = new Schema({
-
-  didCollect: { type: Boolean, default: false, required: true },
-
-  //! can't be named 'collection' (mongoDB/mongoose reserved word)
-  collected: { type: [Number], required: false }, // not required (if did not collect),
+module.exports = new Schema(
   
+  {
+    didCollect: { type: Boolean, default: false, required: true },
 
-  tilePlace: {
-    type: {
-      row: {type: Number, required: true },
-      col: {type: Number, required: true },
-      tile: {type: [], required: true } //? refactor ? it is now mixed type because of 'special'
-    },
-    required: true
-  }
+    //! can't be named 'collection' (mongoDB/mongoose reserved word)
+    collected: { type: [Number], required: false }, // not required (if did not collect),
+    
+    tilePlace: {
+      type: {
+        row: {type: Number, required: true },
+        col: {type: Number, required: true },
+        tile: {type: [], required: true } //? refactor ? it is now mixed type because of 'special'
+      },
+      required: true
+    }
+  },
 
-});
+  // Schema options
+  { _id: false}
+
+);
