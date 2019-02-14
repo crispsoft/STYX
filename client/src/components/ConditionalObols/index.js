2a3eec6d8ed560466c296c1217df8c32f324e43d
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-14 00:35:13 -0600

    condition circles appearing

--- client/src/components/ConditionalObols/index.js
+++ client/src/components/ConditionalObols/index.js
@@ -8,7 +8,7 @@ function ConditionalObols (props) {
     const type = props.type;
 
     const obolColor = {
-        
+
     }
    
     
@@ -16,35 +16,48 @@ function ConditionalObols (props) {
     switch (type) {
         case "1-all":
            return( <div className="one-allContainer">
-                <div className="one-all" color={App.colorMap[0]} style={{color: `${App.colorMap[0]}`}}/>
-                <div className="one-all" color={App.colorMap[1]}></div>
-                <div className="one-all" color={App.colorMap[2]}></div>
-                <div className="one-all" color={App.colorMap[3]}></div>
-                <div className="one-all" color={App.colorMap[4]}></div>
-                <div className="one-all" color={App.colorMap[5]}></div>
-                <div className="one-all" color={App.colorMap[6]}></div>
+                <div className="one-all" color={App.colorMap[0]}/>
+                <div className="one-all" color={App.colorMap[1]}/>
+                <div className="one-all" color={App.colorMap[2]}/>
+                <div className="one-all" color={App.colorMap[3]}/>
+                <div className="one-all" color={App.colorMap[4]}/>
+                <div className="one-all" color={App.colorMap[5]}/>
+                <div className="one-all" color={App.colorMap[6]}/>
             </div>
            );
         break;
 
         case "3-pair":
-           return( <div className="three-pairContainer">
-                <div className="three-pair"></div>
-                <div className="three-pair"></div>
-                <div className="three-pair"></div>
-                <div className="three-pair"></div>
-                <div className="three-pair"></div>
-                <div className="three-pair"></div>
+           return( 
+           <div>
+                <div className="three-pairContainer">
+                    <div className="three-pair" color={App.colorMap[0]}/>
+                    <div className="three-pair" color={App.colorMap[1]}/>
+                    <div className="three-pair" color={App.colorMap[2]}/>
+                </div>
+                <div className="three-pairContainer">
+                    <div className="three-pair" color={App.colorMap[0]}/>
+                    <div className="three-pair" color={App.colorMap[1]}/>
+                    <div className="three-pair" color={App.colorMap[2]}/>
+                </div>
+
             </div>
            );
         break;
 
         case "4-kind":
-           return ( <div className="four-kindContainer">
-                <div className="four-kind"></div>
-                <div className="four-kind"></div>
-                <div className="four-kind"></div>
-                <div className="four-kind"></div>
+           return (
+            <div>
+                <div className="four-kindContainer">
+                    <div className="four-kind" color={App.colorMap[0]}/>
+                    <div className="four-kind" color={App.colorMap[0]}/>
+                </div>
+                
+                <div className="four-kindContainer">
+                    <div className="four-kind" color={App.colorMap[0]}/>
+                    <div className="four-kind" color={App.colorMap[0]}/>    
+                </div>
+            
             </div>
            );
         break;
