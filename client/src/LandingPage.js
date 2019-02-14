0d00164ea1274586ff9aba4f0f3dd88ae1327abc
Andrew Gaurin <gaurin1107@gmail.com>
2019-02-14 15:16:09 -0600

    have route to game not show nav elements

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,33 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import App from './App';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
 
+
+
 const LandingPage = () => (
   <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-        <li>
-          <Link to="/game">Play!</Link>
-        </li>
-      </ul>
+    <Switch>
+      <Route path="/game" component={App} /> {/*only game will show up*/}
+      <Route component={IncludeNav} />
+    </Switch>
 
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/game" component={App} />
-    </div>
   </Router>
+);
+
+const IncludeNav = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About Us</Link>
+      </li>
+      <li>
+        <Link to="/game">Play!</Link>
+      </li>
+    </ul>
+
+    <hr />
+
+    <Route path="/about" component={About} />
+  </div>
 );
 
 
