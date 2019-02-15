2b993ade59ce664e17f86bac0fdc75bf668236e2
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-14 19:47:46 -0600

    created GameLogin and BeastCard components

---
+++ client/src/components/BeastCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./style.css";
+
+function BeastCard() {
+    return(
+        <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+        </div>
+    ) 
+}
+
+export default BeastCard;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,67 +1,25 @@
 import React from "react";
 import Hero from "../Hero";
+import GameLogin from "../GameLogin";
+import BeastCard from "../BeastCard";
 
 const Home = () => (
   <div>
     <Hero />
-    <div className="hero">
 
-      <div className="hero-img">
-        <div className="game-login-container">
-          <div className="join-game">
-            <h2 className="join-game-text">Join Game</h2>
-            <form>
-              <input type="text" />
-              <submit id="join-game-btn" />
-            </form>
-          </div>
-          <div className="start-game">
-            <h2>Start Game</h2>
-            <button id="start-game-btn">Go</button>
-          </div>
-        </div>
+    <GameLogin />
+
+    <div className="hero-img">
+      <div className="beasts-row">
+        <BeastCard />
+        <BeastCard />
+        <BeastCard />
+        <BeastCard />
       </div>
-
-      <div className="hero-img">
-        <div className="beasts-row">
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-        </div>
-        <div className="beasts-row">
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-          <div className="beast-card">
-            <div className="beast" />
-            <div className="beast-title">Cerberus</div>
-            <div className="beast-description">lorem ipsum</div>
-          </div>
-        </div>
+      <div className="beasts-row">
+        <BeastCard />
+        <BeastCard />
+        <BeastCard />
       </div>
     </div>
   </div>
