03e8763e58967212b0ce5eb9e064b2e778e3b15a
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-14 19:09:21 -0600

    added beast cards

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -13,20 +13,62 @@ const Home = () => (
           <span />
         </div>
       </div>
-      
+
       <div className="hero-img">
         <div className="game-login-container">
-            <div className="join-game">
-              <h2 className="join-game-text">Join Game</h2>
-              <form>
-                <input type="text"></input>
-                <submit id="join-game-btn"></submit>
-              </form>
-            </div>
-            <div className="start-game">
-              <h2>Start Game</h2>
-              <button id="start-game-btn">Go</button>
-            </div>
+          <div className="join-game">
+            <h2 className="join-game-text">Join Game</h2>
+            <form>
+              <input type="text" />
+              <submit id="join-game-btn" />
+            </form>
+          </div>
+          <div className="start-game">
+            <h2>Start Game</h2>
+            <button id="start-game-btn">Go</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero-img">
+        <div className="beasts-row">
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
+        </div>
+        <div className="beasts-row">
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
+          <div className="beast-card">
+            <div className="beast" />
+            <div className="beast-title">Cerberus</div>
+            <div className="beast-description">lorem ipsum</div>
+          </div>
         </div>
       </div>
     </div>
