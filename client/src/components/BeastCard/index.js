99052814b12570234376853d88178851be318248
ashleyxwang <ashleywang514@yahoo.com>
2019-02-20 21:29:11 -0600

    adjust colors on home

--- client/src/components/BeastCard/index.js
+++ client/src/components/BeastCard/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./style.css";
+import colors from "../../constants/colors";
 
 function BeastCard(props) {
     return(
         <div className="beast-card">
-            <img className="beast" src={props.img}/>
+            <img className="beast" src={props.img} style={{background:props.color}}/>
             <h3 className="beast-title">{props.name}</h3>
             <p className="beast-description">{props.description}</p>
         </div>
