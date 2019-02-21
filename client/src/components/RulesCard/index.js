db4f8095e5859949dc5bdef6c8d43501a1a0744d
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-20 21:21:16 -0600

    styled rules modal, started adding content

--- client/src/components/RulesCard/index.js
+++ client/src/components/RulesCard/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import style from "./style.css";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
+// function rand() {
+//   return Math.round(Math.random() * 20) - 10;
+// }
 
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -31,19 +31,35 @@ const styles = theme => ({
 });
 
 class RulesCard extends React.Component {
-    render() {
-        const { classes } = this.props;
-        return (
-          <div className="rulescard" >
-            <Typography variant="h6" id="modal-title">
-              Text in a modal
-            </Typography>
-            <Typography variant="subtitle1" id="simple-modal-description">
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="rulescard">
+        <div className="modal-title">
+          <Typography variant="h3" id="modal-title-text">
+            RULES
+          </Typography>
+        </div>
+        <div className="rules-column-container">
+          <div className="rules-column">
+            <Typography variant="h6" id="rules-title">
+              Turn Sequence
             </Typography>
           </div>
-        );
-    }
+          <div className="rules-column">
+            <Typography variant="h6" id="rules-title">
+              Obol Exchange
+            </Typography>
+            <Typography variant="body1" id="rules-text">
+                <p>Seven Unique</p>
+                <p>Three Pair</p>
+                <p>Four of a Kind</p>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RulesCard;
