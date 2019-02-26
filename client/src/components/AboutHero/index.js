fd11f7ee0657d7a1668072e16a08340557d60b83
NiRuaG <gaurin1107@gmail.com>
2019-02-26 17:32:53 -0600

    cleanup

--- client/src/components/AboutHero/index.js
+++ client/src/components/AboutHero/index.js
@@ -3,22 +3,11 @@ import "./style.css";
 
 class AboutHero extends Component {
 
-  scrollOnClick = function () {
-
-  }
-
-
   render() {
-
     return (
-      <div className="hero">
-        <div className="hero-img">
-          <h1 className="about-header">About Us</h1>
-          <br />
-        </div>
-      </div>
-  );
-}
+      <h1 className="about-header">About Us</h1>
+    );
+  }
 }
 
 export default AboutHero;
