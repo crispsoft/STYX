c55d7f5c540dc5838c02e8bd9fea0b1e3b0746ff
NiRuaG <gaurin1107@gmail.com>
2019-02-26 10:34:58 -0600

    remove `pointer-events:none;`

--- client/src/AppStyles/OppNames.style.js
+++ client/src/AppStyles/OppNames.style.js
@@ -5,7 +5,6 @@ import { offWhite } from './../constants/colors';
 const OppNameBase = styled.div`
   background: ${offWhite};
   
-  pointer-events: none;
   user-select: none;
 
   position: absolute;
@@ -23,18 +22,15 @@ const TopName = styled(OppNameBase)`
   padding: 0.1rem 0.5rem;
   bottom: -1em;
   border-top: none;
-  
 `
 
 const SideName = styled(OppNameBase)`
   writing-mode: vertical-lr;
   text-orientation: upright;
   padding: 0.5rem 0.2rem;
-  
-
 
   /* Chrome (72.0.3626.96) would not vertically center name without this */
-  /* FireFox (65.0) would, but possibly not per spec due to position: absolute?
+  /* FireFox (65.0) would, but possibly not as per spec due to position: absolute? (as part of OppNameBase)
   https://www.w3.org/TR/css-flexbox-1/#abspos-items */
   top: 50%; transform: translate(0,-50%);
 `;
